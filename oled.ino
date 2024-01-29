e7f8c6889f7cf74bce88ebc4148bdb16bdae973b
trotyl <mail@trotyl.xyz>
2024-01-29 18:58:15 -0500

    update oled

--- oled.ino
+++ oled.ino
@@ -1,14 +1,20 @@
+#include <U8g2lib.h>
 
+#define FONT_USED      u8x8_font_pxplusibmcgathin_f
 
-#include "U8g2lib.h"
+U8X8_SSD1306_128X64_NONAME_HW_I2C u8g2(U8X8_PIN_NONE);
 
 void setup() {
-
-  // put your setup code here, to run once:
-
+  u8g2.begin();
+  u8g2.setPowerSave(0);
+  u8g2.setFont(FONT_USED);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  for (uint8_t i = 0; i < 5; i++) {
+    u8g2.setCursor(0, i);
+    u8g2.print("Hello world ");
+    u8g2.print(i);
+    delay(1000);
+  }
 }
